93e4df384be1d9eefa25af2f9ee78ec596f61fde
Andrey Sholinka <sholinka2111@gmail.com>
2021-05-10 13:03:30 +0700

    10/05/21 - first commit

---
+++ src/PhoneModel.js
@@ -0,0 +1,29 @@
+const {
+    Model
+} = require('objection');
+const knex = require('./connection');
+const UserModel = require('./UserModel');
+
+Model.knex(knex);
+
+class PhoneModel extends Model {
+
+    static get tableName() {
+        return 'phone';
+    }
+
+    static get relationMappings() {
+        return {
+            users: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: UserModel,
+                join: {
+                    from: 'phone.user_id',
+                    to: 'users.id'
+                }
+            }
+        }
+    }
+}
+
+module.exports = PhoneModel;
